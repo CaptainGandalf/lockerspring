3a0ea827c75d96eb935ec4d70fe8c0f0f63700df
Skirmantas <skirmantasstanaitis@gmail.com>
2019-10-25 22:55:02 +0200

    Added Test controller and loacl postgre config

---
+++ src/main/java/com/locker/locker/controlers/TestController.java
@@ -0,0 +1,17 @@
+package com.locker.locker.controlers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("/dalykas")
+    public String test() {
+        return "test";
+    }
+}
